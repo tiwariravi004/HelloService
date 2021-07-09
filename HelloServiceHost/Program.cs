e8ed86e045f82b19c974ccdbd4f66aada1ea7ce3
Ravi Tiwari <Ravi Tiwari@DESKTOP-1MP7GLI>
2021-07-09 11:57:23 +0530

    Add project files.

---
+++ HelloServiceHost/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using HelloService;
+
+namespace HelloServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(HelloService.HelloService)))
+            {
+                host.Open();
+                Console.WriteLine("Host Started @" + DateTime.Now.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
